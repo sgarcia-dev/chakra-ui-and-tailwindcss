f97096e3dacc86f3b003a829e6933c146786a8b2
Sergei Garcia <sgarcia.dev@gmail.com>
2022-07-21 17:25:03 -0500

    feat: clean up project

--- src/App.js
+++ src/App.js
@@ -1,45 +1,17 @@
 import React from 'react';
 import {
   ChakraProvider,
-  Box,
-  Text,
-  Link,
-  VStack,
-  Code,
-  Grid,
   theme,
   Button,
   useDisclosure,
 } from '@chakra-ui/react';
-import { ColorModeSwitcher } from './ColorModeSwitcher';
-import { Logo } from './Logo';
 import MyModal from './MyModal';
 
 function App() {
   const { isOpen, onClose, onOpen } = useDisclosure()
   return (
     <ChakraProvider theme={theme}>
-      <Box textAlign="center" fontSize="xl">
-        <Grid minH="100vh" p={3}>
-          <ColorModeSwitcher justifySelf="flex-end" />
-          <VStack spacing={8}>
-            <Logo h="40vmin" pointerEvents="none" />
-            <Text>
-              Edit <Code fontSize="xl">src/App.js</Code> and save to reload.
-            </Text>
-            <Link
-              color="teal.500"
-              href="https://chakra-ui.com"
-              fontSize="2xl"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Learn Chakra
-            </Link>
-            <Button onClick={onOpen}>Click me to Show Modal</Button>
-          </VStack>
-        </Grid>
-      </Box>
+      <Button onClick={onOpen}>Click me to Show Modal</Button>
       <MyModal isOpen={isOpen} onClose={onClose} />
     </ChakraProvider>
   );
